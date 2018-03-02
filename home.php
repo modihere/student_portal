7e0da21c91c13bf0b1362acefc701887045d748f
Vivek <nit.vivek@hotmail.com>
2018-03-03 00:49:10 +0530

    update repo

---
+++ home.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Main Page
+	</title>
+	<link href="https://fonts.googleapis.com/css?family=Noto+Serif" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Lobster+Two" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/home1.css">
+
+
+
+</head>
+
+
+<body background="images/background.jpg">
+
+
+	<div id="header" style="text-align: center;">
+		<img src="images/nit_logo.png" alt="nit_logo" style="float: left; width:100px;height:100px;opacity: 0.7">
+			<h1>NIT DURGAPUR</h1>
+			<h2>(An Institute of National Importance under Government of India, Ministry of Human Resource Development)</h2>
+
+		</div>
+			<div class="container">
+				<ul class="navbar">
+					<li><a href="home.php">Home</a></li>
+					<li>
+						<div class="dropdown">
+							<button class="dropbtn">Login</button>
+							<div class="dropdown-content">
+								<a href="<?php echo 'user_login.php' ?>">Student Login</a>
+								<a href="admin_login.php">Administrator Login</a>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="dropdown">
+							<button class="dropbtn">Signup</button>
+							<div class="dropdown-content">
+							    <a href="user_register.php">Student Signup</a>
+							<!--    <a href="login2.html">Administration Signup</a> --!-->
+						  	</div>
+						</div>
+					</li>
+					 
+				</ul>
+			</div>
+		<div>
+		<div class="col-container">
+			<div class="leftcolumn">
+
+			</div>
+			<div class="image" style="max-width:500px">
+			<img src="images/bg.jpg" alt="" align="middle-left"/>
+	
+      
+				</div>
+			<div class="rightcolumn">
+				<p style="color: #CCCCCC;" >The National Institute of Technology Durgapur also known as NIT Durgapur or NITD, is a Central Government engineering college located in Durgapur, West Bengal, India.
+				</p>
+			</div>
+		</div>
+			<div class="container" style="padding: 0px 0 0 0; margin: 0px; width: 100%">
+
+<h4>
+National Institute of Technology
+<br>
+Durgapur
+</h4>
+</div>
+</div>
+<div class="row" style="background-color:#25252D">
+<div class="col-xs-12">
+<h5 style="text-align:center;margin-top:10px;color:#fff">Copyright © 2018. All Rights Reserved. National Institute of Technology, Durgapur</h5>
+</div>
+</div>
+</div>
+	</div>
+</body>
+</html>
---
+++ includes/connection.php
@@ -0,0 +1,8 @@
+<?php
+$dbservername="localhost";
+$dbusername="root";
+$dbpassword="elementary";
+#create a database in mysql first then execute this file
+$dbname="my_app";
+$conn=mysqli_connect($dbservername,$dbusername,$dbpassword,$dbname) or die("ERROR, couldn't connect." . mysqli_error($conn));
+?>
